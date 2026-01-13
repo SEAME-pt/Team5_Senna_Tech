d680f1e48eefe42f409647a4e317b9ea2872b50b
vivaccar <viniciusvaccarih@hotmail.com>
2026-01-13 18:18:01 +0000

    + | added initial system requirements tests (qt autostart and updates) to use in evidencies

---
+++ tests/system/cluster_auto_start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Pega a primeira linha do log do cluster
+LINE=$(journalctl -b -t cluster-start -o short-monotonic | head -n1)
+
+if [ -z "$LINE" ]; then
+    exit 1
+fi
+
+# Extrai o número entre colchetes
+START_TIME=$(echo "$LINE" | sed -E 's/^\[\s*([0-9]+\.[0-9]+)\].*$/\1/')
+
+LIMIT=15
+
+# Comparação float
+awk -v t="$START_TIME" -v l="$LIMIT" 'BEGIN { if (t <= l) { exit 0 } else { exit 1 } }'
+exit $?
