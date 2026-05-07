070dff255e79059c2e19ecc2ec21cb32521eb9ba
vivaccar <viniciusvaccarih@hotmail.com>
2026-05-07 14:13:44 +0100

    + | fixed INC in prepare.sh

--- tests/unit/ThreadX_RTOS/pca9685/prepare.sh
+++ tests/unit/ThreadX_RTOS/pca9685/prepare.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
-set -e
+set -eux
 
 GREEN="\033[0;32m"
 NC="\033[0m"
 
-# pega o diretório real onde o script está
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 EXPECTED_SUFFIX="tests/unit/ThreadX_RTOS/pca9685"
 
-# valida path
 if [[ "$SCRIPT_DIR" != *"$EXPECTED_SUFFIX" ]]; then
     echo "❌ Safety check failed!"
     echo "This script must run inside tests/unit/ThreadX_RTOS/pca9685"
     echo "Current path: $SCRIPT_DIR"
     exit 1
-fi  
+fi
 
 SRC_DIR="$SCRIPT_DIR/src"
+INC_DIR="$SCRIPT_DIR/inc"
 
-# proteção extra: garantir que existe e não é vazio
 if [[ ! -d "$SRC_DIR" || "$SRC_DIR" == "/" ]]; then
     echo "❌ Invalid src directory"
     exit 1
 fi
 
+mkdir -p "$SRC_DIR"
+mkdir -p "$INC_DIR"
+
 rm -rf "$SRC_DIR"/*
+rm -rf "$INC_DIR"/*
 
 cp -rf "$SCRIPT_DIR/../../../../src/threadx/SennaTech/Core/Src/." "$SRC_DIR/"
 cp -rf "$SCRIPT_DIR/../../../../src/threadx/SennaTech/Core/Inc/." "$INC_DIR/"
