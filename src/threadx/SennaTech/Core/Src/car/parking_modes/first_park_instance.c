ad7579196c685146ae7985232c4b1eb241f797c9
jose5556 <josefilmeneses@gmail.com>
2026-05-20 12:56:58 +0100

    ~ | refactor name of the files

---
+++ src/threadx/SennaTech/Core/Src/car/parking_modes/first_park_instance.c
@@ -0,0 +1,26 @@
+#include "car_modes.h"
+
+UINT confirm_first_maneuver(t_ultrasonic_data ultrasonic_data)
+{
+    if (ultrasonic_data.back_distance_cm > FIRST_MANEUVER_SAFE_DISTANCE_CM)
+        return 0; // Not close enough to the back wall
+    return 1; // Close enough to the back wall
+}
+
+e_parking_mode park_first_maneuver(car_t *car)
+{
+    t_ultrasonic_data ultrasonic_data;
+    bzero(&ultrasonic_data, sizeof(ultrasonic_data));
+
+    while (tx_queue_receive(&g_ultrasonic_data_queue, &ultrasonic_data, TX_WAIT_FOREVER) == TX_SUCCESS)
+    {
+        if (!is_distance_safe(ultrasonic_data.back_distance_cm, 0))
+            return ERROR_MANEUVER;
+
+        car_set_throttle_percent(car, -0.05f, 0); // Move backwards really slowly
+
+        if (confirm_first_maneuver(ultrasonic_data))
+            return SECOND_MANEUVER;
+    }
+    return ERROR_MANEUVER;
+}
