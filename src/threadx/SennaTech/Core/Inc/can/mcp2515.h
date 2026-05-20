7648a61fabcbfa06414f41471090943ce1d5c952
jose5556 <josefilmeneses@gmail.com>
2026-05-20 10:34:02 +0100

    ~ | translation commenst to english

--- src/threadx/SennaTech/Core/Inc/can/mcp2515.h
+++ src/threadx/SennaTech/Core/Inc/can/mcp2515.h
@@ -14,7 +14,7 @@
 #define MCP2515_CS_LOW()   HAL_GPIO_WritePin(GPIOE, GPIO_PIN_12, GPIO_PIN_RESET)
 #define MCP2515_CS_HIGH()  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_12, GPIO_PIN_SET)
 
-// Comandos SPI
+// SPI commands
 #define MCP_RESET     0xC0
 #define MCP_READ      0x03
 #define MCP_WRITE     0x02
@@ -22,11 +22,7 @@
 #define MCP_READ_RX   0x90
 #define MCP_LOAD_TX   0x40
 
-// comandos INT
-//#define MCP_INT_Pin GPIO_PIN_7
-//#define MCP_INT_GPIO_Port GPIOC
-
-// Registradores MCP2515
+// MCP2515 Registers
 #define MCP_CANSTAT   0x0E
 #define MCP_CANCTRL   0x0F
 
@@ -34,14 +30,14 @@
 #define MCP_CNF2      0x29
 #define MCP_CNF3      0x28
 
-// Registradores de TX
+// TX Registers
 #define MCP_TXB0CTRL  0x30
 #define MCP_TXB0SIDH  0x31
 #define MCP_TXB0SIDL  0x32
 #define MCP_TXB0DLC   0x35
 #define MCP_TXB0D0    0x36
 
-// Registradores de RX
+// RX Registers
 #define MCP_RXB0CTRL  0x60
 #define MCP_RXB0SIDH  0x61
 #define MCP_RXB0SIDL  0x62
@@ -54,12 +50,12 @@
 #define MCP_RXB1DLC   0x75
 #define MCP_RXB1D0    0x76
 
-// Registradores de Interrupção
+// Interrupt registers
 #define MCP_CANINTF   0x2C
 #define MCP_CANINTE   0x2B
 #define MCP_EFLG      0x2D
 
-// Máscaras e Filtros
+// Masks and Filters
 #define MCP_RXM0SIDH  0x20
 #define MCP_RXM0SIDL  0x21
 #define MCP_RXM1SIDH  0x24
@@ -67,6 +63,8 @@
 
 #define MODE_NORMAL 0x00
 #define MODE_CONFIG 0x80
+
+extern SPI_HandleTypeDef hspi1;
 
 typedef enum {
     MCP_8MHZ = 0,
@@ -76,14 +74,13 @@ typedef enum {
     CAN_500KBPS = 0,
 } CAN_Bitrate;
 
-/* Estrutura de frame CAN */
+/* CAN frame struct */
 typedef struct {
     uint32_t id;
     uint8_t dlc;
     uint8_t data[8];
 } CAN_Frame;
 
-/* Funções públicas */
 HAL_StatusTypeDef MCP2515_Reset(void);
 HAL_StatusTypeDef MCP2515_SetBitrate(CAN_Bitrate bitrate, MCP_Clock clock);
 HAL_StatusTypeDef MCP2515_SetNormalMode(void);
@@ -91,11 +88,8 @@ HAL_StatusTypeDef MCP2515_SendMessage(CAN_Frame *frame);
 HAL_StatusTypeDef MCP2515_ReceiveMessage(CAN_Frame *frame);
 HAL_StatusTypeDef MCP2515_Init(void);
 
-// Funções auxiliares (se necessário)
+// utils
 uint8_t MCP2515_ReadByte(uint8_t address);
 void MCP2515_WriteByte(uint8_t address, uint8_t data);
-
-/* SPI deve ser declarada como extern */
-extern SPI_HandleTypeDef hspi1;
 
 #endif /* MCP2515_H_ */
